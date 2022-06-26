1fc5e13ce15fb535fa5e9e69d7bf91548a683a00
github tomo-333 <tomoka.i.rainbow@gmail.com>
2022-06-26 20:48:39 +0900

    diff

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Git Diff'),
     );
   }
 }

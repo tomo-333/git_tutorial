45b2af7d4d8763ade008ad522d0ed0f1a1429288
github tomo-333 <tomoka.i.rainbow@gmail.com>
2022-06-27 09:24:50 +0900

    master.dartを追加

---
+++ lib/master.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class master extends StatelessWidget {
+  const master({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
